4d1a0471fb4eda378d9fbf85ac327372c2e392a5
willxj36 <willxj36@gmail.com>
2021-01-18 21:56:52 -0700

    finished lab including advanced

--- public/app.js
+++ public/app.js
@@ -1,1 +1,5 @@
-alert('Yo');
+let div = document.getElementById('js-entry');
+let h1 = document.createElement('h1');
+let text = document.createTextNode('Hello there!');
+div.append(h1);
+h1.append(text);
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 let app = express();
 const path = require('path');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
 // app.get('/', (req, res) => {
 //     res.send('Hello from the server side!');
 // });
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     console.log(req.url);
@@ -12,5 +16,25 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.post('/submission', (req, res) => {
+    let rawArray = fs.readFileSync('./server/formsubmissions.json');
+    let contactsArray = JSON.parse(rawArray);
+    let submission = {
+        "Name": req.body.name,
+        "Phone Number": req.body.phonenumber
+    };
+    contactsArray.push(submission);
+    console.log(contactsArray);
+    fs.writeFileSync('./server/formsubmissions.json', JSON.stringify(contactsArray));
+    res.send('Form submitted!');
+});
+
+app.get('/formsubmissions', (req, res) => {
+    let json = fs.readFileSync('./server/formsubmissions.json');
+    let result = JSON.parse(json);
+    res.send(result);
+    }
+)
 
 app.listen(3000);
